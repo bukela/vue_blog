f01c18ac57006d121545511685d06d3e75173fc6
bukela <bukela@gmail.com>
2018-02-26 16:06:55 +0100

    blog input

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
+import addBlog from './components/addBlog.vue';
 export default {
+  components: {
+    'add-blog': addBlog
+  },
   data () {
     return {
       
